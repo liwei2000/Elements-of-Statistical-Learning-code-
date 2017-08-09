14c0002bffa70d150f0090d4e8838373a85b004f
Wei Li <wei_li92@yahoo.com>
2017-08-09 19:00:45 -0400

    run logistic regression with L1 penality

--- chapter4/logistic_regression.py
+++ chapter4/logistic_regression.py
@@ -2,6 +2,7 @@ import seaborn as sns
 import pandas as pd
 from statsmodels.discrete.discrete_model import Logit
 from statsmodels.tools.tools import add_constant
+from sklearn.linear_model import LogisticRegression
 
 d = pd.read_csv('/home/wei/projects/esl/data/SAheart.data.txt', index_col='row.names')
 preds = d.iloc[:, :-1]
@@ -14,3 +15,19 @@ print(f.summary())
 # generate a scatter plot
 sns.pairplot(d)
 
+# use L1 penalization
+fr = lm.fit_regularized(method='l1', disp=False, alpha=0.1)
+print(fr.summary())
+
+# vary the L1 penalty
+N = 30
+alps = logspace(0, 4, N)
+b = []
+for i, alpha in enumerate(alps):
+    fr = lm.fit_regularized(method='l1', disp=False, alpha=alpha)
+    b.append(fr.params)
+
+b = pd.DataFrame(b, index=alps)
+print(b)
+
+b.set_index(nansum(b.abs(), axis=1)).iloc[::-1, 1:].plot(logx=True, title='regularized regression beta')
